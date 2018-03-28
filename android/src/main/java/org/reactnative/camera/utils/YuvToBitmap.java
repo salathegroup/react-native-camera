b6b3cff18274164d44883ad8883f7de3832fc688
Boris Conforty <boris@conforty.net>
2018-03-28 16:57:11 +0200

    Renamed class. Updated YUV to bitmap class

--- android/src/main/java/org/reactnative/camera/utils/YuvToBitmap.java
+++ android/src/main/java/org/reactnative/camera/utils/YuvToBitmap.java
@@ -11,32 +11,39 @@ import android.renderscript.ScriptIntrinsicYuvToRGB;
  * Created by Boris Conforty on 26.03.18.
  */
 
-public class YuvToRGB {
+public class YuvToBitmap {
     private Context mContext;
     private RenderScript mRenderScript;
     private ScriptIntrinsicYuvToRGB mYuvToRgbIntrinsic;
     private Allocation aIn, aOut;
     private Bitmap mBitmap;
 
-    public YuvToRGB(Context context) {
+    public YuvToBitmap(Context context) {
         mContext = context;
     }
 
     private void prepare(int width, int height) {
-        if (mRenderScript == null) {
+        boolean renderScriptNeeded = mRenderScript == null;
+        boolean bitmapNeeded = mBitmap == null || mBitmap.getWidth() != width || mBitmap.getHeight() != height;
+        boolean inNeeded = aIn == null || (mBitmap != null && mBitmap.getWidth() * mBitmap.getHeight() != width * height);
+
+        if (renderScriptNeeded) {
             mRenderScript = RenderScript.create(mContext);
             mYuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(mRenderScript, Element.U8_4(mRenderScript));
         }
-        if (mBitmap == null || mBitmap.getWidth() != width || mBitmap.getHeight() != height) {
+        if (bitmapNeeded) {
+            mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            aOut = Allocation.createFromBitmap(mRenderScript, mBitmap);  // this simple !
+        }
+        if (inNeeded) {
             int yuvDatalength = width * height * 3 / 2;  // 12 bits per pixel
             aIn = Allocation.createSized(mRenderScript, Element.U8(mRenderScript), yuvDatalength);
-
-            mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
-            aOut = Allocation.createFromBitmap(mRenderScript, mBitmap);  // this simple !
-
             mYuvToRgbIntrinsic.setInput(aIn);
         }
+    }
+
+    public Allocation getOut() {
+        return aOut;
     }
 
     public Bitmap refreshBitmap(byte[] data, int width, int height) {
